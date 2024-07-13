051dbe238d88fdca3a27ae81f16529953101cb60
farouq-gfishi <farouqgfishi@gmail.com>
2024-07-13 15:31:09 +0300

    feat: add the backup script

---
+++ backups.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+add_file() {
+        local parent_id=$1
+        local file_path=$2
+        local file_name=$(awk -F "/" '{print $NF}' <<< "$file_path")
+        local files=$(gdrive files list --parent $parent_id | awk 'NR > 1 {print $2}')
+        if echo "$files" | grep -q "$file_name"; then
+                local file_id=$(gdrive files list --query "'$parent_id' in parents" | awk -v name="$file_name" '$2 == name {print $1}')
+                gdrive files update $file_id $file_path
+        else
+                gdrive files upload --parent $parent_id $file_path
+        fi
+}
+
+add_folder() {
+        local parent_id=$1
+        local folder_path=$2
+        local folder_name=$(awk -F "/" '{print $NF}' <<< "$folder_path")
+        echo $folder_path
+        local files=$(gdrive files list --parent $parent_id | awk 'NR > 1 {print $2}')
+
+        if ! echo "$files" | grep -q "$folder_name"; then
+                local message=$(gdrive files mkdir --parent $parent_id $folder_name)
+                local current_id=$(awk '{print $NF}' <<< "$message")
+                main $current_id $folder_path
+        else
+                local folder_id=$(gdrive files list --query "'$parent_id' in parents" | awk -v name="$folder_name" '$2 == name {print $1}')
+                main $folder_id $folder_path
+        fi
+}
+
+main() {
+        local parent_id=$1
+        local folder_path=$2
+        for item in "$folder_path"/*; do
+                if [ -f $item ]; then
+                        add_file $parent_id $item
+                elif [ -d $item ]; then
+                        add_folder $parent_id $item
+                fi
+        done
+}
+
+parent_id=17MAifV92Rj-4Q78cQGotsxRx0AsSwUrj
+folder_path=/home/farouq-gfishi/Personal/devops
+
+echo
+echo "***************************************"
+echo
+date
+echo
+echo "***************************************"
+echo
+
+begin=$EPOCHREALTIME
+main $parent_id $folder_path
+end=$EPOCHREALTIME
+elapsed=$(bc -l <<< "$end - $begin")
+
+echo
+echo "***************************************"
+echo DONE!
+echo "It takes $elapsed seconds to complete."
+
+exit 0
